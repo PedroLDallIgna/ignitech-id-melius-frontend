6fd4c222ebd8efca6699c30fc26a3e4fdbba2d1d
Pedro Lucas Dall' Igna <pedro.lucas-di@hotmail.com>
2023-10-01 00:46:54 -0300

    feat: updates form with validation

--- src/app/form-funcionario/form-funcionario.component.ts
+++ src/app/form-funcionario/form-funcionario.component.ts
@@ -1,10 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FuncionariosService } from '../funcionarios.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-funcionario',
@@ -12,20 +9,53 @@ import {
   styleUrls: ['./form-funcionario.component.scss'],
 })
 export class FormFuncionarioComponent {
-  createFuncionarioForm = this.formBuilder.group({
-    Nome: ``,
-    CPF: ``,
-    DataNascimento: ``,
-    Endereco: ``,
-    Telefone: ``,
-    Email: ``,
-    Sexo: ``,
-    Equipe: ``,
-  });
+  createFuncionarioForm = this.formBuilder.group(
+    {
+      nome: this.formBuilder.control(``, [Validators.required]),
+      cpf: this.formBuilder.control(``, [
+        Validators.required,
+        Validators.pattern(/\d{3}\.\d{3}\.\d{3}\-\d{2}/),
+      ]),
+      dataNascimento: this.formBuilder.control(``, [Validators.required]),
+      endereco: this.formBuilder.control(``),
+      telefone: this.formBuilder.control(``, [
+        Validators.pattern(/\(\d{2}\)\d{5}-\d{4}/),
+      ]),
+      email: this.formBuilder.control(``, [
+        Validators.required,
+        Validators.email,
+      ]),
+      sexo: this.formBuilder.control(``, [Validators.required]),
+      equipe: this.formBuilder.control(``, {
+        validators: [Validators.pattern(/[\d]/)],
+      }),
+    },
+    {
+      updateOn: 'blur',
+    }
+  );
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private funcionariosService: FuncionariosService,
+    private router: Router
+  ) {}
 
   onSubmit() {
-    console.log(this.createFuncionarioForm.value);
+    if (this.createFuncionarioForm.valid) {
+      const data = {
+        nome: this.createFuncionarioForm.value.nome,
+        cpf: this.createFuncionarioForm.value.cpf,
+        dataNascimento: this.createFuncionarioForm.value.dataNascimento,
+        sexo: this.createFuncionarioForm.value.sexo,
+        email: this.createFuncionarioForm.value.email,
+        telefone: this.createFuncionarioForm.value.telefone || null,
+        endereco: this.createFuncionarioForm.value.endereco || null,
+        equipe: Number(this.createFuncionarioForm.value.equipe) || null,
+      };
+      this.funcionariosService.postFuncionario(data).subscribe({
+        next: () => this.router.navigate(['funcionarios']),
+      });
+    }
   }
 }
--- src/app/funcionarios.service.ts
+++ src/app/funcionarios.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Funcionario } from './funcionario';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,5 +13,9 @@ export class FuncionariosService {
     return this.http.get<Funcionario[]>(
       'http://localhost:3000/api/funcionarios'
     );
+  }
+
+  public postFuncionario<T>(data: T) {
+    return this.http.post('http://localhost:3000/api/funcionarios', data);
   }
 }
